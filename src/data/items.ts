aae1c6db7c2c28c0cf1bec509da0328dea114cc0
Eliabe Gai <eliabe.gai@clinicorp.com>
2023-09-24 04:06:47 -0300

    sistema fincanceiro

---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from '../types/Item'
+
+export const items: Item[] = [
+  {
+    date: new Date(2023, 8, 6),
+    category: 'food',
+    title: 'McDonalds',
+    value: 32.12
+  },
+  {
+    date: new Date(2023, 8, 13),
+    category: 'food',
+    title: 'Burger King',
+    value: 28
+  },
+  {
+    date: new Date(2023, 8, 23),
+    category: 'rent',
+    title: 'Aluguel Apt',
+    value: 2300
+  },
+  {
+    date: new Date(2023, 10, 18),
+    category: 'salary',
+    title: 'Salario ACME',
+    value: 4500
+  }
+]
